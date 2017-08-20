f357dc2682fb30c412a2edc73f37ed5a57b93703
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-08-20 17:08:40 -0400

    Add methods to translate name of months in spanish to month number

--- utils.py
+++ utils.py
@@ -6,3 +6,12 @@ def to_unicode(obj, encoding='utf-8'):
         if not isinstance(obj, unicode):
             obj = unicode(obj, encoding)
     return obj
+
+
+def translate_spanish_month_letter_to_number(month_letter):
+    months = ['enero', 'febrero', 'marzo', 'abril', 'mayo',
+              'junio', 'julio', 'agosto', 'septiembre',
+              'octubre', 'noviembre', 'diciembre']
+    for idx in range(0, 12):
+        if months[idx] == month_letter:
+            return idx + 1
