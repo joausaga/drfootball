12501f312e20af398fb95bbd36c6880cd6be9429
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-08-23 17:42:52 -0400

    Add function to normalize spanish text

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 __author__ = 'jorgesaldivar'
 
+import unicodedata
+
 
 def to_unicode(obj, encoding='utf-8'):
     if isinstance(obj, basestring):
@@ -15,3 +17,6 @@ def translate_spanish_month_letter_to_number(month_letter):
     for idx in range(0, 12):
         if months[idx] == month_letter:
             return idx + 1
+
+def normalize_text(text):
+    return unicodedata.normalize('NFD', to_unicode(text)).encode('ascii', 'ignore')
