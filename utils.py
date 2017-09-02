9c02a0e16a114d9f9f2791e499d1c41df2133d9a
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-09-02 17:04:52 -0400

    Improve the method that translates spanish month names to month numbers

--- utils.py
+++ utils.py
@@ -11,12 +11,16 @@ def to_unicode(obj, encoding='utf-8'):
 
 
 def translate_spanish_month_letter_to_number(month_letter):
-    months = ['enero', 'febrero', 'marzo', 'abril', 'mayo',
-              'junio', 'julio', 'agosto', 'septiembre',
-              'octubre', 'noviembre', 'diciembre']
-    for idx in range(0, 12):
-        if months[idx] == month_letter:
-            return idx + 1
+    months = {'enero' : 1, 'febrero' : 2,
+              'marzo': 3, 'abril': 4,
+              'mayo': 5, 'junio': 6,
+              'julio': 7, 'agosto': 8,
+              'septiembre': 9, 'setiembre': 9,
+              'octubre': 10, 'noviembre': 11,
+              'diciembre': 12}
+    for month_name, month_number in months.items():
+        if month_name == month_letter:
+            return month_number
 
 def normalize_text(text):
     return unicodedata.normalize('NFD', to_unicode(text)).encode('ascii', 'ignore')
