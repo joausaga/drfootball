d2eba5f76a45248dfbab71f613e6db6d4819ad72
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-09-10 18:31:23 -0400

    Add two new methods: one to format text before saving into the database and another to add a thousand separation character (comma or point) to integers

--- utils.py
+++ utils.py
@@ -33,3 +33,21 @@ def csv_to_dict(csv_file):
         csv_dict = list(csv_reader)
 
     return csv_dict
+
+
+def format_text_to_save_db(text, encoding='utf-8'):
+    return text.title().encode(encoding)
+
+
+# Taken from
+# https://stackoverflow.com/questions/1823058/how-to-print-number-with-commas-as-thousands-separators
+def int_seperation_char(x, sep_char=','):
+    if type(x) not in [type(0), type(0L)]:
+        raise TypeError('Parameter must be an integer')
+    if x < 0:
+        return '-' + int_seperation_char(-x)
+    result = ''
+    while x >= 1000:
+        x, r = divmod(x, 1000)
+        result = sep_char + '%03d%s' % (r, result)
+    return '%d%s' % (x, result)
