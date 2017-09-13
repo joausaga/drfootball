cf62a1a4d430ee7c0ba9a6d3143bab0577d0adfc
Jorge Saldivar <jorgesaldivar@gmail.com>
2017-09-13 17:18:27 -0400

    Improve the admin panel of the tournament model and add new admin panels

--- defense/admin.py
+++ defense/admin.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 from models import Source, Country, Region, City, Stadium, Player, Coach, \
                    Referee, Team, PlayerTeam, CoachTeam, StadiumTeam, \
                    Tournament, TournamentTeam, TournamentPlayer, Game, \
-                   GameTeam, GamePlayer, GameReferee, Goal, Card
+                   GameTeam, GamePlayer, GameReferee, Goal, Card, SeasonTeamFinalStatus, \
+                   TournamentStatus
 from django.contrib import admin
 from goalkeeper import parser_rsssf
 from goalkeeper.wiki_scrapers import ParaguayanChampionshipResultsScraper, \
@@ -78,7 +79,7 @@ class SourceAdmin(admin.ModelAdmin):
 
 
 class TournamentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'country', 'start_date', 'end_date', 'champion', 'runnerup')
+    list_display = ('name', 'country', 'start_date', 'end_date', 'champion', 'runnerup', 'season_champion')
     actions = ['collect_extra_info', 'collect_results']
 
     def champion(self, obj):
@@ -87,6 +88,7 @@ class TournamentAdmin(admin.ModelAdmin):
             if tournament_team.final_position == 1:
                 return tournament_team.team.name
         return ''
+    champion.short_description = "Winner"
 
     def runnerup(self, obj):
         tournament_teams = obj.tournamentteam_set.all()
@@ -136,7 +138,7 @@ class TournamentAdmin(admin.ModelAdmin):
         elif len(city_objs) > 1:
             city = disambiguate_objs_by_name(city_objs, city_name)
         else:
-            raise Exception('Error!, Dont understand the number of cities')
+            raise Exception('Dont understand the number of cities')
         self.__update_city(city, city_dict)
         return city
 
@@ -159,7 +161,7 @@ class TournamentAdmin(admin.ModelAdmin):
         elif len(stadium_objs) > 1:
             stadium = disambiguate_objs_by_name(stadium_objs, stadium_name)
         else:
-            raise Exception('Error!, Dont understand the number of stadiums')
+            raise Exception('Dont understand the number of stadiums')
         self.__update_stadium(stadium, stadium_dict)
         return stadium
 
@@ -172,20 +174,26 @@ class TournamentAdmin(admin.ModelAdmin):
 
     def __get_or_create_team(self, team, source):
         team_name = utils.normalize_text(team['name'])
+        team_name = team_name.replace('club', '').strip()  # delete word 'club'
         team_objs = Team.objects.filter(name__icontains=team_name)
-        stadium = self.__get_or_create_stadium(team['stadium'], team['city'])
+        stadium = None
         if len(team_objs) == 1:
             team_obj = team_objs[0]
         elif len(team_objs) > 1:
             team_obj = disambiguate_objs_by_name(team_objs, team_name)
         elif len(team_objs) == 0:
             # the team doesn't exist yet
-            team_obj = create_new_team(team, stadium)
+            if 'stadium' in team.keys() and 'city' in team.keys():
+                stadium = self.__get_or_create_stadium(team['stadium'], team['city'])
+                team_obj = create_new_team(team, stadium)
+            else:
+                raise Exception('The team {0} doesnt exist and a new one cannot be created because there are not '
+                                'information about city and stadium'.format(team_name))
         else:
-            raise Exception('Error!, Dont understand the number of teams')
+            raise Exception('Dont understand the number of teams')
         # associate the team with its stadium in case the association
         # doesn't exists
-        if not team_obj.stadium.all():
+        if stadium and not team_obj.stadium.all():
             StadiumTeam.objects.create(stadium=stadium, team=team_obj, source=source)
         self.__update_team(team_obj, team)
         return team_obj
@@ -209,15 +217,46 @@ class TournamentAdmin(admin.ModelAdmin):
                     TournamentTeam.objects.create(tournament=tournament_obj,
                                                   team=team_obj,
                                                   source=source_obj)
+            if 'season statuses' in information_collected:
+                for team in tournament_info['teams']:
+                    # create the teams' season final status
+                    team_obj = self.__get_or_create_team(team, source_obj)
+                    ss = SeasonTeamFinalStatus.objects.create(team=team_obj, season=obj.year,
+                                                              position=int(team['position']),
+                                                              points=int(team['points']),
+                                                              games=int(team['games']),
+                                                              wins=int(team['won']),
+                                                              draws=int(team['drew']),
+                                                              losses=int(team['lost']),
+                                                              goals=int(team['gf']),
+                                                              goals_conceded=int(team['gc']),
+                                                              goals_difference=int(team['goals_difference']))
+                    if team['international_status']:
+                        for status in team['international_status']:
+                            status_obj, created = TournamentStatus.objects.get_or_create(name=status)
+                            ss.status.add(status_obj)
+        self.message_user(request, "The action was completed successfully!")
     collect_extra_info.short_description = "Collect Extra Information"
 
     def collect_results(self, request, queryset):
-        pass
+        DEF_PREFIX_TOURNAMENT_FILE = 'campeonatos'
+
+        for obj in queryset:
+            tournament_dict = {
+                'name': obj.name,
+                'year': obj.year,
+                'start_string': obj.start_string,
+                'end_string': obj.end_string,
+                'results_local_fname': '{0}{1}.html'.format(DEF_PREFIX_TOURNAMENT_FILE, obj.year)
+            }
+            parser_rsssf.read_championship_results(tournament_dict)
+        self.message_user(request, "The action was completed successfully!")
     collect_results.short_description = "Collect Tournament Results"
 
 
 class TournamentTeamAdmin(admin.ModelAdmin):
     list_display = ('tournament', 'team', 'final_position')
+    list_filter = ('tournament', )
 
 
 class TeamAdmin(admin.ModelAdmin):
@@ -294,6 +333,20 @@ class StadiumAdmin(admin.ModelAdmin):
     display_capacity.short_description = 'Capacity'
 
 
+class SeasonTeamFinalStatusAdmin(admin.ModelAdmin):
+    list_display = ('position', 'team', 'season', 'final_status', 'points', 'games', 'wins', 'draws',
+                    'losses', 'goals', 'goals_conceded', 'goals_difference')
+    ordering = ('position', 'season')
+    list_filter = ('season', )
+
+    def final_status(self, obj):
+        statuses = obj.status.all()
+        str_final_status = ''
+        for status in statuses:
+            str_final_status = status.name.title() + ' '
+        return str_final_status
+    final_status.short_description = 'International Status'
+
 admin.site.register(Source, SourceAdmin)
 admin.site.register(Country)
 admin.site.register(Region)
@@ -309,6 +362,8 @@ admin.site.register(CoachTeam)
 admin.site.register(Tournament, TournamentAdmin)
 admin.site.register(TournamentTeam, TournamentTeamAdmin)
 admin.site.register(TournamentPlayer)
+#admin.site.register(TournamentStatus)
+admin.site.register(SeasonTeamFinalStatus, SeasonTeamFinalStatusAdmin)
 admin.site.register(Game, GameAdmin)
 admin.site.register(GameTeam, GameTeamAdmin)
 admin.site.register(GamePlayer)
